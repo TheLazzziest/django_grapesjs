53256cc6dd061f1a43058771f4c29325914c4557
TheLazzziest <mxyakovenko9@gmail.com>
2019-10-15 18:20:45 +0300

    Redefine test structure.

--- django_grapesjs/tests/__init__.py
+++ django_grapesjs/tests/__init__.py
@@ -1,4 +1,4 @@
-from .forms import *
-from .models import *
+from .test_form_fields import *
+from .test_model_fields import *
 from .utils import *
 from .views import *
---
+++ django_grapesjs/tests/test_form_fields.py
@@ -0,0 +1,66 @@
+from mock import mock
+from django import test
+from django.core.exceptions import ValidationError
+from django_grapesjs.forms import GrapesJsField, GrapesJsWidget
+
+__all__ = ('GrapesJsFieldTestCase', )
+
+
+class GrapesJsFieldTestCase(test.TestCase):
+    list_args = {
+        'default_html': lambda inst: inst.widget.default_html,
+        'html_name_init_conf': lambda inst: inst.widget.html_name_init_conf,
+        'apply_django_tag': lambda inst: inst.widget.apply_django_tag,
+        'template_choices': lambda inst: inst.widget.template_choices,
+        'validate_tags': lambda inst: inst.validate_tags
+    }
+
+    def get_check_dict_attr(self, values_default, dict_values, inst):
+        data = {}
+
+        for k, v in self.list_args.items():
+            data[k] = values_default or dict_values.get(k) or v(inst)
+
+        return data
+
+    def test_init(self):
+        incorrect_value = 'string'
+
+        formfield = GrapesJsField()
+
+        self.assertSetEqual(
+            set(GrapesJsField.__init__.__defaults__),
+            set(self.get_check_dict_attr(None, {}, formfield).values())
+        )
+
+        data = self.get_check_dict_attr(incorrect_value, {}, None)
+        formfield = GrapesJsField(**data)
+
+        self.assertSetEqual(
+            set(data.values()),
+            set(self.get_check_dict_attr(None, {}, formfield).values())
+        )
+
+    def test_validate(self):
+        formfield = GrapesJsField()
+
+        self.assertIsNone(formfield.validate('string'))
+
+    def test_clean_correct_value(self):
+        value = 'string'
+
+        formfield = GrapesJsField()
+        result = formfield.clean(value)
+
+        self.assertEqual(value, result)
+
+    def test_clean_incorrect_value(self):
+        with self.assertRaises(ValidationError):
+            formfield = GrapesJsField()
+            formfield.clean('')
+
+        with self.assertRaises(TypeError):
+            formfield = GrapesJsField()
+            formfield.clean(0)
+
+
