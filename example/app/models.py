a94f8a40ff66430582e58809c20d90ebe135930d
TheLazzziest <mxyakovenko9@gmail.com>
2019-10-15 18:24:12 +0300

    Adjust a set of parent models in GrapesJSJSONModel declaration.

--- example/app/models.py
+++ example/app/models.py
@@ -1,8 +1,9 @@
-from django.contrib.postgres.fields import HStoreField
 from django.utils.translation import ugettext as _
 from django.db import models
 
-from django_grapesjs.models.base import BaseGrapesJSModel
+from django_grapesjs.models.base import (
+    BaseGrapesJSModel, BaseMetaGrapesJSModel, ModelToDictMixin
+)
 from django_grapesjs.models.fields import GrapesJsHtmlField
 
 
@@ -10,12 +11,12 @@ class GrapesJSModel(models.Model):
     html = GrapesJsHtmlField()
 
 
-class GrapesJSJSONModel(BaseGrapesJSModel):
-    gjs_assets = HStoreField()
-    gjs_css = HStoreField()
-    gjs_styles = HStoreField()
-    gjs_html = HStoreField()
-    gjs_components = HStoreField()
+class GrapesJSJSONModel(ModelToDictMixin,
+                        BaseMetaGrapesJSModel,
+                        BaseGrapesJSModel):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name = _('grapes model')
