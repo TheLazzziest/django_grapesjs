fd2745a5b5b862d9b1d2b41e4abc4f0a54d608d3
TheLazzziest <mxyakovenko9@gmail.com>
2019-10-15 18:18:31 +0300

    Separate base form to mixin and additional meta data model.

--- django_grapesjs/models/base.py
+++ django_grapesjs/models/base.py
@@ -1,20 +1,32 @@
-from django.contrib.postgres.fields import JSONField
+from django.contrib.postgres.fields import HStoreField
 from django.db import models
 from django.forms import model_to_dict
 
 
-class BaseGrapesJSModel(models.Model):
-    gjs_assets = JSONField(null=True)
-    gjs_css = JSONField(null=True)
-    gjs_styles = JSONField(null=True)
-    gjs_html = JSONField(null=True)
-    gjs_components = JSONField(null=True)
+class ModelToDictMixin(object):
+    """Model mixin to convert Model object to a dict."""
 
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    def to_dict(self):
+        return model_to_dict(self)
+
+
+class BaseMetaGrapesJSModel(models.Model):
+    """Basic model with meta data fields for a template."""
+    title = models.CharField(max_length=255, null=True)
+    url = models.URLField(null=True)
 
     class Meta:
         abstract = True
 
-    def to_dict(self):
-        return model_to_dict(self)
+
+class BaseGrapesJSModel(models.Model):
+    """Basic model for storing a template data."""
+
+    gjs_assets = HStoreField(null=True)
+    gjs_css = HStoreField(null=True)
+    gjs_styles = HStoreField(null=True)
+    gjs_html = HStoreField(null=True)
+    gjs_components = HStoreField(null=True)
+
+    class Meta:
+        abstract = True
